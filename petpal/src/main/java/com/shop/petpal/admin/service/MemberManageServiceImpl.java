b97ff12742998ec2f4f9211c9618abe9b7075468
user <user@DESKTOP-69K6101>
2024-07-22 16:39:10 +0900

    다송

--- petpal/src/main/java/com/shop/petpal/admin/service/MemberManageServiceImpl.java
+++ petpal/src/main/java/com/shop/petpal/admin/service/MemberManageServiceImpl.java
@@ -24,9 +24,30 @@ public class MemberManageServiceImpl implements MemberManageService {
         return memberManageMapper.findById(memberNum);
     }
 
+    
     @Override
     public void updateMember(Member member) {
-        memberManageMapper.updateMember(member);
+    	try {
+            // 이메일이 부분적으로 입력된 경우 이메일 전체를 생성하여
+    		
+            if (member.getEmail().length() != 0) {
+            	member.setEmail(member.getEmail());
+            }
+
+            // 전화번호가 부분적으로 입력된 경우 전화번호 전체를 생성하여 설
+            if (member.getTel1().length() != 0 && member.getTel2().length() != 0 && member.getTel3().length() != 0) {
+            	member.setTel(member.getTel1() + "-" + member.getTel2() + "-" + member.getTel3());
+            }
+
+            // 회원 정보를 member1 테이블에 업데이트
+            // 패스워드가 변경된 경우에만 업데이트하도록 해야 하지만, 현재 조건이 없음
+            memberManageMapper.updateMember(member);
+            
+        } catch (Exception e) {
+            // 예외 발생 시 스택 트레이스를 출력하고 예외를 다시 던짐
+            e.printStackTrace();
+            throw e;
+        }        
     }
 }
 
