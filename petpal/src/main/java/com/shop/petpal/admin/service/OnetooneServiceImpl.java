f5ff12ee458e998a764a7aaba408fc920cb9e999
chlal <chlals61@naver.com>
2024-07-23 16:44:46 +0900

    상품리스트 재고(강사님이 고쳐줌) 지렸다

---
+++ petpal/src/main/java/com/shop/petpal/admin/service/OnetooneServiceImpl.java
@@ -0,0 +1,20 @@
+package com.shop.petpal.admin.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.shop.petpal.admin.domain.OnetooneManage;
+
+@Service
+public class OnetooneServiceImpl implements OnetooneManageService{
+
+	@Override
+	public List<OnetooneManage> listOnetoone(Map<String, Object> map) {
+		List<OnetooneManage> list = null;
+		
+		return list;
+	}
+
+}
